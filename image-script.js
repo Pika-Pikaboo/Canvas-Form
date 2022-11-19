c54460dd548cf469b409eed60649b52984d96f9a
pikaboo <stevinjack1ina1000@gmail.com>
2022-11-19 14:26:16 +0630

    added alternative way with  FileReader Interface

--- image-script.js
+++ image-script.js
@@ -8,3 +8,15 @@ file.addEventListener('change', () => {
 		URL.revokeObjectURL(imgUrl);
 	}
 });
+
+/* alternative way with FileReader interface */
+
+/*file.addEventListener('change', (event) => {
+	const reader = new FileReader();
+	const [imgFile] = event.target.files;
+	// const imgFile = event.target.files.item(0);
+	reader.addEventListener('load', () => {
+		image.src = reader.result;
+	});
+	reader.readAsDataURL(imgFile);
+});*/
